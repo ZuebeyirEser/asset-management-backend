e24f50fe43b4c02c0c13e8de02d04fbb56545337
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-14 13:41:13 +0200

    added employee  find by id rest api

--- src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
+++ src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.listofemployee.demo.Controller;
 
+import com.listofemployee.demo.Exceptions.ResourceNotFoundExceptions;
 import com.listofemployee.demo.Model.Employee;
 import com.listofemployee.demo.Model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,16 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return  employeeRepository.save(employee);
+    }
+
+    // get employee by id
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee>  getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptions("Employee does not exist with id:" + id)
+        );
+        return ResponseEntity.ok(employee);
+
     }
 
 
