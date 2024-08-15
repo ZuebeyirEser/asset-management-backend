089c9c5a56d0c4e63530c00383044f1ef3e1c568
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-15 16:32:46 +0200

    added updateEmployee rest api for editing ui

--- src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
+++ src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
@@ -13,8 +13,12 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
@@ -33,7 +37,17 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundExceptions("Employee does not exist with id:" + id)
         );
         return ResponseEntity.ok(employee);
-
+    }
+    // updating the rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptions("Employee does not exist with id:" + id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 
