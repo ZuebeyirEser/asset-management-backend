dd196e0b47e3e567281f4f69ff2633f3ac5e6d84
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-16 14:52:41 +0200

    minor fix

--- src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
+++ src/main/java/com/listofemployee/demo/Controller/EmployeeController.java
@@ -3,12 +3,10 @@ package com.listofemployee.demo.Controller;
 import com.listofemployee.demo.Exceptions.ResourceNotFoundExceptions;
 import com.listofemployee.demo.Model.Employee;
 import com.listofemployee.demo.Model.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
@@ -38,7 +36,7 @@ public class EmployeeController {
         );
         return ResponseEntity.ok(employee);
     }
-    // updating the rest api
+    // updating the rest api r
     @PutMapping("/employees/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails) {
         Employee employee = employeeRepository.findById(id)
@@ -49,13 +47,13 @@ public class EmployeeController {
         Employee updatedEmployee = employeeRepository.save(employee);
         return ResponseEntity.ok(updatedEmployee);
     }
+    // HTTP 204 No Content status
     @DeleteMapping("employees/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundExceptions("Employee does not exist with id: " + id));
 
         employeeRepository.deleteById(id);
-        // HTTP 204 No Content status
         return ResponseEntity.noContent().build();
     }
 
