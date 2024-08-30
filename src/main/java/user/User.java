5310813143f1c75ed47f2f5fd9096bc2a0415728
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-30 14:16:59 +0200

    created user package and class

---
+++ src/main/java/user/User.java
@@ -0,0 +1,30 @@
+package user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/* @Data is a convenient shortcut annotation that bundles the features of
+@ToString, @EqualsAndHashCode, @Getter / @Setter and @RequiredArgsConstructor
+together:*/
+@Data
+/*The @Builder annotation is part of Project Lombok, a library that reduces
+boilerplate code in Java. Project Lombok achieves this by
+generating common code constructs, such as getters, setters,
+constructors, and builders, during compile time. The @Builder
+annotation, specifically, generates a builder pattern for a class,
+allowing you to create instances of the class with a fluent and expressive API.*/
+@Builder
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
+
+}
