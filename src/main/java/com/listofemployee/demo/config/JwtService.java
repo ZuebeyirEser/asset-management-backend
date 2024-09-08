449220481e3f30af11873c682dd594853ccb40a9
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-09-08 09:48:38 +0200

    JwtService added

--- src/main/java/com/listofemployee/demo/config/JwtService.java
+++ src/main/java/com/listofemployee/demo/config/JwtService.java
@@ -1,10 +1,40 @@
 package com.listofemployee.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.apache.tomcat.util.buf.UDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private String secretKey;
+    @Value("${app.secret.key}")
+    private static String SECRET_KEY;
     public String extractUserEmail(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimResololver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResololver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
