ada45065ffc645527484f191d5ac8edc641494fd
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-09-20 15:08:50 +0200

    added webconfig file for COSR

---
+++ src/main/java/com/listofemployee/demo/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.listofemployee.demo.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:3000"); // Add your frontend URL
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter();
+    }
+}
